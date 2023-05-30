b474533009ba49bc526aa8c095ac02076db00a78
asistenciasTEC <134986838+asistenciasTEC@users.noreply.github.com>
2023-05-30 16:32:14 -0600

    otro cambio para gh-pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -87,7 +87,7 @@ const Login = () => {
                     errorButton1: null,
                     loading: false,
                 });
-                history("/");
+                history("/asistencias/");
             } catch (error) {
                 if (error.message === "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).") {
                     setData({
